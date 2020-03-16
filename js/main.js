e9e942b1a6e0d1c33a6bd881ffa7b1deee9609e9
lau-gonzalez <laau.gonzaalez@hotmail.com>
2020-03-16 09:26:22 -0300

    Subiendo Archivos

---
+++ js/main.js
@@ -0,0 +1,178 @@
+$(document).ready(function(){
+
+
+  RewriteCond %{HTTP_HOST} ^www.site\.com$ [NC] RewriteRule ^(.*)$ Http://site.com/$1 [R=301,L]
+
+
+  //Funcion DesplazamientoSlide
+
+
+  $(function(){
+
+     $('a[href*=servicios]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
+
+    $(function(){
+
+     $('a[href*=nosotros]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
+
+
+  $(function(){
+
+     $('a[href*=inicio]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
+
+
+    $(function(){
+
+     $('a[href*=contacto]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
+
+
+     $(function(){
+
+     $('a[href*=proyectos]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
+
+
+    $('#consultar').click(function(){
+      e.preventDefault();
+      e.stopImmediatePropagation();
+
+
+     $.ajax({
+              url: 'process.php',
+              type: 'POST',
+              data: $('#contacto').serialize(),
+              success: function() {
+                
+                
+                $('#contacto').hide();
+                $('#contacto').append("<p class='thanks'>Gracias por contactarnos. Nos comunicaremos a la brevedad</p>")
+             
+               }
+
+       });
+    });
+             
+   
+});
+
+
+
+
+
+
+
+
